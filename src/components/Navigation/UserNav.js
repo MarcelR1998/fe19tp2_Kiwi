2dabe568b5cf194a2d0a07a145259dc4ec71fd01
NataliaWiman <natalia.wiman@gmail.com>
2020-03-03 10:30:49 +0100

    Added user navigation

---
+++ src/components/Navigation/UserNav.js
@@ -0,0 +1,155 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import SignOutButton from "../SignOut";
+import styled from "styled-components";
+import { AuthUserContext } from "../Session";
+
+class UserNav extends Component {
+  state = {
+    show: false
+  };
+
+  show = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <UserInfo>
+            <UserCard>
+              {this.state.show && (
+                <CardWrapper>
+                  <User>
+                    <UserImg src="https://66.media.tumblr.com/a667b9f6ba81810d3b608b5fd2dbdc19/c09b191bc35ed178-7b/s1280x1920/0c0ca5bdbfb10eb94f8a4f039e1d33479da22b79.jpg"></UserImg>
+                    <UserData>
+                      <UserNames>{authUser.username}</UserNames>
+                      <UserEmail>{authUser.email}</UserEmail>
+                    </UserData>
+                  </User>
+                  <CardList>
+                    <ListItem>
+                      <CardLink account to={ROUTES.ACCOUNT}>
+                        My Account
+                      </CardLink>
+                    </ListItem>
+                    <ListItem>
+                      <CardLink account to={ROUTES.ACCOUNT}>
+                        Settings
+                      </CardLink>
+                    </ListItem>
+                    <ListItem>
+                      <SignOutButton />
+                    </ListItem>
+                  </CardList>
+                </CardWrapper>
+              )}
+            </UserCard>
+            <UserBtn
+              onClick={() => {
+                this.show();
+              }}
+            >
+              <UserName>{authUser.username}</UserName>
+              <i class="fas fa-angle-down"></i>
+            </UserBtn>
+          </UserInfo>
+        )}
+      </AuthUserContext.Consumer>
+    );
+  }
+}
+
+const UserCard = styled.div``;
+
+const CardWrapper = styled.div`
+  position: absolute;
+  top: 70px;
+  right: 15px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
+`;
+
+const User = styled.div`
+  padding: 15px 25px;
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+`;
+
+const UserData = styled.div``;
+
+const UserNames = styled.span`
+  padding-right: 5px;
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 24px;
+  font-weight: bold;
+`;
+
+const UserName = styled.span`
+  padding-right: 5px;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 24px;
+  font-weight: bold;
+`;
+
+const UserEmail = styled.div`
+  font-size: 12px;
+  color: #555;
+`;
+
+const CardList = styled.ul`
+  background-color: #f8f8f8;
+  list-style: none;
+  margin: 0;
+  padding: 0 0 10px;
+`;
+
+const ListItem = styled.li``;
+
+const CardLink = styled(Link)`
+  display: block;
+  padding: 10px 15px;
+  color: #333;
+  text-decoration: none;
+  &:hover {
+    background-color: #f2f2f2;
+    transition: background-color 0.3s;
+  }
+`;
+
+const UserInfo = styled.div`
+  margin: 10px 0 10px 50px;
+  display: flex;
+`;
+
+const UserImg = styled.img`
+  height: 70px;
+  width: 70px;
+  padding: 10px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const UserBtn = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 0;
+  background-color: #f2f2f2;
+  cursor: pointer;
+  color: #333;
+  &:hover {
+    color: #666;
+    transition: color 0.3s linear;
+  }
+`;
+
+export default UserNav;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import SignOutButton from "../SignOut";
+import UserNav from "../Navigation/UserNav";
 
 import * as ROUTES from "../../constants/routes";
 import { AuthUserContext } from "../Session";
@@ -10,63 +11,160 @@ import * as ROLES from "../../constants/roles";
 import styled from "styled-components";
 
 const Navigation = () => (
-    <Nav>
-        <AuthUserContext.Consumer>
-            {authUser =>
-                authUser ? (
-                    <NavigationAuth authUser={authUser} />
-                ) : (
-                        <NavigationNonAuth />
-                    )
-            }
-        </AuthUserContext.Consumer>
-    </Nav>
+  <Nav>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser ? (
+          <NavigationAuth authUser={authUser} />
+        ) : (
+          <NavigationNonAuth />
+        )
+      }
+    </AuthUserContext.Consumer>
+  </Nav>
 );
 
 const NavigationAuth = ({ authUser }) => (
-    <NavList>
+  <NavWrapper>
+    <NavTop>
+      <NavBanner>LOGO</NavBanner>
+      <NavUser>
+        <TopSearch>
+          <SearchInput placeholder="Search"></SearchInput>
+        </TopSearch>
+        <UserNav />
+        {/* <UserBtn>
+            <UserImg src="https://via.placeholder.com/60"></UserImg>
+            <UserName>{authUser.username}</UserName>
+            <i class="fas fa-angle-down"></i>
+          </UserBtn>
+        </UserNav> */}
+      </NavUser>
+    </NavTop>
+    <NavSide>
+      <NavList>
+        {/* <NavItem>
+      <NavLink to={ROUTES.LANDING}>Landing</NavLink>
+    </NavItem> */}
         <NavItem>
-            <NavLink to={ROUTES.LANDING}>Landing</NavLink>
+          <NavLink home to={ROUTES.HOME}>
+            Home
+          </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink home to={ROUTES.HOME}>
-                Home
-      </NavLink>
+          <NavLink news to={ROUTES.HOME}>
+            News
+          </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink account to={ROUTES.ACCOUNT}>
-                Account
-      </NavLink>
+          <NavLink portfolio to={ROUTES.HOME}>
+            Portfolio
+          </NavLink>
         </NavItem>
+        {/*         <NavItem>
+          <NavLink account to={ROUTES.ACCOUNT}>
+            Account
+          </NavLink>
+        </NavItem> */}
         {authUser.roles.includes(ROLES.ADMIN) && (
-            <NavItem>
-                <NavLink admin to={ROUTES.ADMIN}>
-                    Admin
-        </NavLink>
-            </NavItem>
+          <NavItem>
+            <NavLink admin to={ROUTES.ADMIN}>
+              Admin
+            </NavLink>
+          </NavItem>
         )}
-        <NavItem>
-            <SignOutButton />
-        </NavItem>
-    </NavList>
+        {/*         <NavItem>
+          <SignOutButton />
+        </NavItem> */}
+      </NavList>
+    </NavSide>
+  </NavWrapper>
 );
 const NavigationNonAuth = () => (
-    <NavList>
-        <NavItem>
-            <NavLink to={ROUTES.LANDING}>Landing</NavLink>
-        </NavItem>{" "}
-        <NavItem>
-            <NavLink signin to={ROUTES.SIGN_IN}>
-                Sign In
+  <div>
+    {/* <NavList>
+    <NavItem>
+      <NavLink to={ROUTES.LANDING}>Landing</NavLink>
+    </NavItem>{" "}
+    <NavItem>
+      <NavLink signin to={ROUTES.SIGN_IN}>
+        Sign In
       </NavLink>
-        </NavItem>
-    </NavList>
+    </NavItem>
+  </NavList> */}
+  </div>
 );
 
 const Nav = styled.div`
-position: fixed;
-  height: 100vh;
+  /* position: fixed;
+  height: 100vh; */
   background-color: #039be5;
+`;
+
+const NavWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const NavBanner = styled.div`
+  grid-area: topbanner;
+  width: 120px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  flex-shrink: 0;
+  background-color: #039be5;
+`;
+
+const NavUser = styled.div`
+  padding: 0 25px;
+  position: relative;
+  flex-grow: 1;
+  height: 60px;
+  background-color: #f2f2f2;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const NavTop = styled.div`
+  grid-area: topnav;
+  /* position: fixed;
+  top: 0;
+  right: 0;
+  margin-left: 300px; */
+  display: flex;
+`;
+
+const NavSide = styled.div`
+  grid-area: navside;
+  width: 120px;
+  height: 100%;
+  flex-grow: 1;
+  background-color: #039be5;
+`;
+
+const TopSearch = styled.div`
+  flex-grow: 1;
+`;
+
+const SearchInput = styled.input`
+  /* width: 100%; */
+  width: 20%;
+  padding: 8px 15px;
+  font-size: 14px;
+  background-color: #fff;
+  border: 0;
+  border-radius: 10px;
+  outline: 0;
+  &:focus {
+    width: 100%;
+    transition: width 0.6s;
+  }
 `;
 
 const NavList = styled.ul`
@@ -104,19 +202,56 @@ const NavLink = styled(Link)`
     font-size: 24px;
     
     content: "${props => {
-        if (props.home) {
-            return "\f201";
-        } else if (props.account) {
-            return "\f007";
-        } else if (props.admin) {
-            return "\f502";
-        } else if (props.signin) {
-            return "\f2f6";
-        } else {
-            return "\f059";
-        }
+      if (props.home) {
+        return "\f009";
+      } else if (props.portfolio) {
+        return "\f200";
+      } else if (props.news) {
+        return "\f1ea";
+      } else if (props.account) {
+        return "\f007";
+      } else if (props.admin) {
+        return "\f502";
+      } else if (props.signin) {
+        return "\f2f6";
+      } else {
+        return "\f059";
+      }
     }}";
   }
 `;
+
+/* const UserInfo = styled.div`
+  margin: 10px 0 10px 50px;
+  display: flex;
+`;
+
+const UserImg = styled.img`
+  height: 40px;
+  width: 40px;
+  margin-right: 10px;
+  border-radius: 50%;
+`;
+
+const UserBtn = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 0;
+  background-color: #f2f2f2;
+  cursor: pointer;
+  color: #333;
+  &:hover {
+    color: #666;
+    transition: color 0.3s linear;
+  }
+`;
+
+const UserName = styled.span`
+  padding-right: 5px;
+  font-size: 12px;
+  line-height: 24px;
+  font-weight: bold;
+`; */
 
 export default Navigation;

d39d96bb2e85870c936b3313e839eb542a899e92
emilsundberg-h <emil.a.sundberg@gmail.com>
2020-03-05 09:39:22 +0100

    local storage and wonder beautiful data

--- src/components/ApiReader/index.js
+++ src/components/ApiReader/index.js
@@ -1,41 +1,32 @@
 import React from 'react';
 import { countries } from './miscData.js';
-
 import { apiKey, baseUrl, stockExchange } from '../../constants/urls';
-
 import { filterSymbol } from './usStocks';
-
 import { companyObjects, urlKeys, urlValues, hej, companyNamesFunc, companyObjectsFunc, stocklist } from './recommendationTrends.js'; // {AAPL: http...}
 import { sendMessage } from '../../constants/functions';
 import axios from 'axios';
 import rateLimit from 'axios-rate-limit';
 import StockCard from '../StockCard';
-
 import Charts from '../Charts'
 /* import UserStockList from '../UserStockList'; */
-
 import { withFirebase } from '../Firebase';
 import { unstable_batchedUpdates } from 'react-dom';
-import Search from '../Search/index.js';
-
-
 //const http = rateLimit(axios.create(), { maxRequests: 2, perMilliseconds: 4000, maxRPS: 2 })
 const http = rateLimit(axios.create(), { maxRequests: 2, perMilliseconds: 1000, maxRPS: 2 })
 http.getMaxRPS() // 2
-
 let companySymbols = Object.keys(companyObjects); // ['AAPL',...]
-
 let lastSymbol = companySymbols[companySymbols.length - 1];
-
 let lastUrl = urlKeys[urlKeys.length - 1];
 let urls = Object.values(companyObjects); // [['Http:...'...],['Http:...'...]]
 let historyData = [];
-
+let userStocks;
+let lastSymbols;
 class ApiReader extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            masterObject: {}
+            masterObject: {},
+            Loaded: false
         };
         this.companySymbols = companySymbols;
         this.lastSymbol = lastSymbol;
@@ -43,15 +34,16 @@ class ApiReader extends React.Component {
         this.buildMasterObject = this.buildMasterObject.bind(this);
         this.historyData = historyData;
         this.getSymbolData = this.getSymbolData.bind(this);
+        this.lastSymbols = lastSymbols;
         //this.changeTime = this.changeTime.bind(this)
     }
-
     buildMasterObject() {
-        return this.state.masterObject
+        if (this.state.Loaded) {
+            userStocks = this.state.masterObject;
+            return userStocks
+        }
     }
-
     componentDidMount() {
-
         if (this.props.uid) {
             this.props.firebase.user(this.props.uid).on('value', snapshot => {
                 const stockObject = snapshot.val();
@@ -63,14 +55,9 @@ class ApiReader extends React.Component {
                     const companySymb = Object.keys(companyObj)
                     //urls = Object.values(companyObjects)
                     //console.log(urls)
-
                     this.getSymbolData(companyObj, companySymb);
-                    /* let newStocklist = stocklist.push({
-                        name: 'Gustaf',
-                        symbol: 'jjj'
-                    })
-            
-                    console.log(newStocklist, stocklist) */
+                    lastSymbols = Object.keys(companyObj)[Object.keys(companyObj).length - 1];
+                    return lastSymbols
                 }
             });
         } else { console.log('error') }
@@ -81,35 +68,30 @@ class ApiReader extends React.Component {
         })
         console.log(stocklist)
         this.props.firebase.user(this.props.uid).update({ stocklist: stocklist }) */
-
-
-
-
     }
     componentWillUnmount() {
         this.props.firebase.user(this.props.uid).off();
     }
-
     async getSymbolData(companyObj, companySymb) {
         /*   console.log(companyObj)
           console.log(companySymb) */
-
         // todo: read users stocks from firebase
         /* let flattenedUrl = urls.flat();  */// [url,url,url]
         const masterObject = {}
         //const urlKeys = ['quotes', 'priceTargets', 'news', 'recs'];
         companySymb.forEach(symbol => { // ['AA', 'AAPL',]
             ////console.log (companyObjects[symbol])
-
             let urls = companyObj[symbol];
-
             urls.map(async (url, index) => {
                 const legend = urlKeys[index];
                 try {
                     const result = await http.get(url)
                     masterObject[symbol] = { ...masterObject[symbol], [legend]: result.data }
-                    /*  console.log(masterObject); */
-                    this.setState({ masterObject: masterObject })
+                    console.log(masterObject);
+                    this.setState({
+                        masterObject: masterObject,
+                        Loaded: true
+                    })
                 } catch (error) {
                     console.log(error);
                 }
@@ -143,17 +125,24 @@ class ApiReader extends React.Component {
     render() {
         /* const authUser = this.props.uid; */
         /* console.log(authUser); */
-        console.log(this.props.uid);
-
+        console.log(lastSymbols)
+        if (this.state.masterObject[lastSymbols]) {
+            if (Object.keys(JSON.parse(localStorage.getItem('data'))).toString() !== Object.keys(this.state.masterObject).toString()) {
+                localStorage.setItem('data', JSON.stringify(this.state.masterObject));
+                console.log('local storage run')
+            } else { console.log('hello') }
+        }
         if (this.state.masterObject[lastSymbol]) {
+            console.log(Object.keys(JSON.parse(localStorage.getItem('data'))).map(key => Object.keys(this.state.masterObject).some(e => e === key)))
+        }
+        if (/* this.state.masterObject[lastSymbol] */ this.state.Loaded) {
             /*    console.log(this.lastSymbol) */
-            let dataPoints = this.state.masterObject;
+            /*  let dataPoints = this.buildMasterObject();
+             console.log(dataPoints) */
             /* console.log(this.state.masterObject.map(stock => stock)) */
             /*  console.log(dataPoints['AAPL']['quoteUrl'].h);
              console.log(Object.values(dataPoints));
              console.log(Object.keys(this.buildMasterObject()), Object.values(this.buildMasterObject()).map(data => data.priceTarget)) */
-
-
             /*    this.historyData.push(
                    {
                        A: dataPoints['A']['quoteUrl'],
@@ -168,7 +157,6 @@ class ApiReader extends React.Component {
                        date: dataPoints['AAPL']['quoteUrl'].t
                    }) */
             /* console.log(historyData.forEach(data => console.log(data['A']))) */
-
             /*    let chartData = {
                    labels: ['A', 'AA', 'AAPL'],
                    datasets: [
@@ -183,25 +171,19 @@ class ApiReader extends React.Component {
                        }
                    ]
                }; */
-
-
-
+            let result = localStorage.getItem('data')
+            console.log(JSON.parse(result))
             return (
                 <div>
                     {this.state.masterObject[this.lastSymbol] ? this.state.masterObject[this.lastSymbol].hasOwnProperty(this.lastUrl) ? 'HEJ' : '' : ''}
                     {/*  <Charts chartData={chartData} /> */}
                     <StockCard data={this.state.masterObject} history={historyData} />
-                    {/*    <Search masterObject={this.state.masterObject} /> */}
                     {/*  <UserStockList></UserStockList> */}
                 </div>
             )
-
         } else {
             return ""
         }
     }
-
 }
-
-
 export default withFirebase(ApiReader);
